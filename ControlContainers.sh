0cddfe4b2ba401ed81af6e82e6fd807209b67e89
Sypovik <nekich0999@gmail.com>
2024-11-29 16:30:58 +0700

    отступы

--- ControlContainers.sh
+++ ControlContainers.sh
@@ -5,8 +5,10 @@ DB=db
 
 if [ $1 == "start" ]; then
     docker-compose up -d --build
+
 elif [ $1 == "stop" ]; then
     docker-compose down
+
 elif [ $1 == "rmExited" ]; then
     # Остановить и удалить APP и DB контейнеры
     docker stop $(docker ps -aq --filter "name=$APP"\
@@ -15,10 +17,13 @@ elif [ $1 == "rmExited" ]; then
     docker rm $(docker ps -aq --filter "name=$APP"\
     --filter "name=$DB" --filter "status=exited"\
     2>/dev/null)
+
 elif [ $1 == "appShell" ]; then
     docker exec -it $APP /bin/bash
+
 elif [ $1 == "dbShell" ]; then
     docker exec -it $DB psql -U postgres -d respakdb
+
 elif [ $1 == "runMvn" ]; then
     docker exec -it $APP mvn clean spring-boot:run
 fi
