4820ee65cf12c797cecc3c8405e800dce02fde3d
Sypovik <nekich0999@gmail.com>
2024-11-30 06:26:37 +0700

    Удаление привязанных volum

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 if [ "$1" == "start" ]; then
     docker-compose up --build
 elif [ "$1" == "stop" ]; then
-    docker-compose down
+    docker-compose down -v
 elif [ "$1" == "restart" ]; then
     docker-compose down
     docker-compose up --build
